f615ea1c01f3dce3fd49b86b8737a2d92121ce96
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-21 09:24:29 +0300

    Add user in export

--- src/app/services/ghub.service.ts
+++ src/app/services/ghub.service.ts
@@ -2,10 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from "../User";
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class GhubService {
+  user!:User;
 
   constructor(private http:HttpClient) { }
 }
