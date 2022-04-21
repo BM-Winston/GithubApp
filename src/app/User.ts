ee6354d9bfc6138e50a02ebe721eda45a24d2279
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-21 08:04:37 +0300

    Add base url and access token

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,8 +1,8 @@
-export class User{
-    login!:string;
-    name!:string;
-    avatar_url!:string;
-    bio!:string;
-    followers!:number;
-    following!:number;
+export interface User{
+    login:string;
+    name:string;
+    avatar_url:string;
+    bio:string;
+    followers:number;
+    following:number;
 }
