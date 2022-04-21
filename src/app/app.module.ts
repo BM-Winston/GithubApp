2d26954ea8a8be75b34134f90599b8475ed99b2b
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-04-21 07:47:00 +0300

    rename service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from "@angula/common/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 
